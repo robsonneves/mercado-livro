44fc99db2d2671cbb4d31b1c0a3ae3627adf5072
robsonneves <robsonnevesrodrigues@yahoo.com.br>
2025-11-12 13:07:48 -0300

    Add crud - customer controller.

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # mercado-livro
+- Java 25
+- Kotlin
+- Gradle
+- Spring-boot 3.5.7
+
+# Spring
+- Spring-web
---
+++ src/main/kotlin/com/mercadolivro/model/CustomerModel.kt
@@ -0,0 +1,7 @@
+package com.mercadolivro.model
+
+data class CustomerModel(
+    val id: String,
+    var name: String,
+    var email: String
+)
